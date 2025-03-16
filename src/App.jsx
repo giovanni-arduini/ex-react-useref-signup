5bdfd4d50f08bda3431d11b0f2f12f3c16696667
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-16 18:53:15 +0100

    ADD handleSubmit function ì

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,35 @@ function App() {
   const [experienceYears, setExperienceYears] = useState("");
   const [comments, setComments] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (
+      experienceYears < 0 ||
+      spec === "" ||
+      name === "" ||
+      username === "" ||
+      password === ""
+    ) {
+      console.log("Compila tutti i campi");
+      return;
+    } else {
+      console.log({
+        name,
+        username,
+        password,
+        spec,
+        experienceYears,
+      });
+    }
+  }
+
   return (
     <>
-      <form action="" style={{ display: "flex", flexDirection: "column" }}>
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
         <input
           type="text"
           name="nome_completo"
@@ -18,20 +44,46 @@ function App() {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-        <input type="text" name="username" placeholder="Username" />
+
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
         <input type="password" name="password" placeholder="Password" />
-        <select name="specs" id="specs">
+
+        <select
+          value={spec}
+          onChange={(e) => setSpec(e.target.value)}
+          name="specs"
+          id="specs"
+        >
+          <option value="">-- Seleziona la specializzazione --</option>
           <option value="frontend">Frontend</option>
           <option value="backend">Backend</option>
           <option value="fullstack">Fullstack</option>
         </select>
+
         <input
           type="number"
           name="experience_years"
           id="experience_years"
           placeholder="Anni di esperienza"
+          value={experienceYears}
+          onChange={(e) => setExperienceYears(e.target.value)}
         />
-        <input type="textarea" name="comments" placeholder="Commenti" />
+
+        <input
+          type="textarea"
+          name="comments"
+          placeholder="Commenti"
+          value={comments}
+          onChange={(e) => setComments(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Invia Form</button>
       </form>
     </>
   );
